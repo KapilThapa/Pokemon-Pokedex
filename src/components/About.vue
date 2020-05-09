c21226419078b6dd04f1a24de624c04cb49ba7f7
KapilThapa <kapil_thapa50@yahoo.com>
2020-05-09 21:47:57 +0545

    about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,6 +4,14 @@
         <div class="pokedex-about-content">
             <div class="poke-list">
                 <h1>About</h1>
+                <h4 class="mb-15">Built with</h4>
+                <ul class="technology">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    <li>JQuery</li>
+                    <li>Vue Js</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -14,3 +22,15 @@ export default {
     
 }
 </script>
+
+<style>
+.mb-15{
+    margin-bottom: 15px;
+}
+.technology{
+    list-style: none;
+}
+.technology li{
+    margin-bottom: 10px;
+}
+</style>
